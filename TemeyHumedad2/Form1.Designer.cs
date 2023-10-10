f05f5c5dd61344505e44f49b80b6bd6c5dcbbe7a
IvetteMo <tp984@correo.tij.ibero.mx>
2023-10-10 15:12:42 -0700

    Agregar archivos de proyecto.

---
+++ TemeyHumedad2/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace TemeyHumedad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBaud = new System.Windows.Forms.ComboBox();
+            this.txtCom = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblHumedad = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TemeyHumedad2.Properties.Resources.Captura_de_pantalla_2023_02_25_194024;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(377, 365);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtBaud);
+            this.groupBox1.Controls.Add(this.txtCom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 391);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 121);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ajustes Puerto COM";
+            // 
+            // txtBaud
+            // 
+            this.txtBaud.FormattingEnabled = true;
+            this.txtBaud.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.txtBaud.Location = new System.Drawing.Point(133, 80);
+            this.txtBaud.Name = "txtBaud";
+            this.txtBaud.Size = new System.Drawing.Size(121, 32);
+            this.txtBaud.TabIndex = 3;
+            this.txtBaud.Text = "57600";
+            // 
+            // txtCom
+            // 
+            this.txtCom.FormattingEnabled = true;
+            this.txtCom.Location = new System.Drawing.Point(133, 33);
+            this.txtCom.Name = "txtCom";
+            this.txtCom.Size = new System.Drawing.Size(121, 32);
+            this.txtCom.TabIndex = 2;
+            this.txtCom.DropDown += new System.EventHandler(this.txtCom_DropDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Puerto COM";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(275, 400);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(114, 53);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(275, 459);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(114, 53);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblHumedad);
+            this.panel1.Controls.Add(this.lblTemp);
+            this.panel1.Location = new System.Drawing.Point(396, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(574, 70);
+            this.panel1.TabIndex = 4;
+            // 
+            // lblHumedad
+            // 
+            this.lblHumedad.AutoSize = true;
+            this.lblHumedad.Font = new System.Drawing.Font("Arial Rounded MT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHumedad.Location = new System.Drawing.Point(347, 20);
+            this.lblHumedad.Name = "lblHumedad";
+            this.lblHumedad.Size = new System.Drawing.Size(219, 30);
+            this.lblHumedad.TabIndex = 1;
+            this.lblHumedad.Text = "Humedad=__%RH";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Arial Rounded MT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.Location = new System.Drawing.Point(20, 20);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(241, 30);
+            this.lblTemp.TabIndex = 0;
+            this.lblTemp.Text = "Temperatura= __ °C";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(396, 88);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperatura";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Humedad";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(574, 289);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Temperatura y Humedad";
+            this.chart1.Titles.Add(title1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(510, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(351, 41);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Visual C# - Arduino";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(518, 478);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(343, 36);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "DHT22/ AM2302 Sensor";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 543);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monitoreo Sensor DHT22";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox txtBaud;
+        private System.Windows.Forms.ComboBox txtCom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblHumedad;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ TemeyHumedad2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace TemeyHumedad2
+{
+    public partial class Form1 : Form
+    {
+        double temperatura = 0, humdad = 0;
+        bool actDato = false;
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnAbrir.Enabled = true;
+            btnCerrar.Enabled = false;
+
+            chart1.Series["Temperatura"].Points.AddXY(1, 1);
+            chart1.Series["Humedad"].Points.AddXY(1, 1);
+        }
+
+        private void txtCom_DropDown(object sender, EventArgs e)
+        {
+            string[] portList = SerialPort.GetPortNames();
+            txtCom.Items.Clear();
+            txtCom.Items.AddRange(portList);
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = txtCom.Text;
+                serialPort1.BaudRate = Convert.ToInt32(txtBaud.Text);
+                serialPort1.Open();
+                btnAbrir.Enabled = false;
+                btnCerrar.Enabled = true;
+
+                chart1.Series["Temperatura"].Points.Clear();
+                chart1.Series["Humedad"].Points.Clear();
+
+                MessageBox.Show("Conexion exitosa a la tarjeta arduino");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                btnAbrir.Enabled = true;
+                btnCerrar.Enabled = false;
+
+
+                MessageBox.Show("Desconectado de la tarjeta arduino");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dataIn = serialPort1.ReadTo("\n");
+            Data_Parsing(dataIn);
+            this.BeginInvoke(new EventHandler(Show_Data));
+        }
+
+        private void Show_Data(object sender, EventArgs e)
+        {
+            if (actDato == true)
+            {
+                lblTemp.Text = string.Format("Temperatura= {0}°C", temperatura.ToString());
+                lblHumedad.Text = string.Format("Humedad= {0}%HR", humdad.ToString());
+
+                chart1.Series["Temperatura"].Points.Add(temperatura);
+                chart1.Series["Humedad"].Points.Add(humdad);
+            }
+        }
+
+        private void Data_Parsing(string dato)
+        {
+            sbyte indexOf_startDataCharacter = (sbyte)dato.IndexOf("@");
+            sbyte indexOfA = (sbyte)dato.IndexOf("A");
+            sbyte indexOfB = (sbyte)dato.IndexOf("B");
+
+            //Si el caracter es A o B, y la "@" existe in el paquete de datos
+            if (indexOfA != -1 && indexOfB != -1 && indexOf_startDataCharacter != -1)
+            {
+                try
+                {
+                    string str_Temperatura = dato.Substring(indexOf_startDataCharacter + 1,
+                        (indexOfA - indexOf_startDataCharacter) - 1);
+
+                    string str_humedad = dato.Substring(indexOfA + 1, (indexOfB - indexOfA) - 1);
+
+                    temperatura = Convert.ToDouble(str_Temperatura);
+                    humdad = Convert.ToDouble(str_humedad);
+
+                    actDato = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                actDato = false;
+            }
+
+        }
+    }
+}
+        
